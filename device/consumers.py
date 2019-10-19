d56f6d239a894942b71cf1f43aad3b85dda1ebd4
GabrielMMelo <gabrielmelocomp@gmail.com>
2019-10-19 10:50:43 -0300

    WS token authentication

--- device/consumers.py
+++ device/consumers.py
@@ -1,5 +1,6 @@
 import json
 
+from rest_framework.authtoken.models import Token
 from channels.db import database_sync_to_async
 from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
 
@@ -8,8 +9,8 @@ from .models import Device
 
 class DeviceConsumer(AsyncWebsocketConsumer):
     @database_sync_to_async
-    def get_device(self, id):
-        return Device.objects.get(pk=id)
+    def get_device(self, **args):
+        return Device.objects.get(**args)
 
     """
     @database_sync_to_async
@@ -34,45 +35,75 @@ class DeviceConsumer(AsyncWebsocketConsumer):
         self.device = await self.get_device(id=self.device_id)
         await self.update_channel(self.channel_name)
         self.device_room = str(self.device.id)
-        await self.channel_layer.group_add(self.device_room, self.channel_name
-                                           )  # one channel per device
 
-        #await self.channel_layer.group_add(str(self.device.place),
-        #                                   self.channel_name
-        #                                   )  # one channel per place
+        await self.channel_layer.group_add(
+            self.device_room, self.channel_name
+        )  # one channel per device
+
+        """
+        await self.channel_layer.group_add(str(self.device.place),
+            self.channel_name
+        )  # one channel per place
+        """
 
         # TODO: one channel for type?
         await self.accept()
 
+    async def authorize(self, text_data_json={}):
+        """ Check if given token is valid for any User and persist him in the scope """
+        if self.scope['user'].id:
+            return True
+
+        try:
+            token_key = text_data_json['token']
+            token = Token.objects.get(key=token_key)
+            self.scope['user'] = token.user
+            return True
+        except Exception as e:
+            print("ERROR in authorize(): ", e)
+            return False
+
+    async def check_owner(self):
+        """ Check if connection user is owner of the connection device """
+        try:
+            await self.get_device(id=self.device_id, owner=self.scope['user'])
+            return True
+        except Exception as e:
+            return False
+
     async def receive(self, text_data=None, bytes_data=None):
         text_data_json = json.loads(text_data)
-        """
-        try:
-            mac = text_data_json['mac']
-            await self.set_mac(mac)
-        except KeyError:
-            pass
-        """
+        authorized = await self.authorize(text_data_json)
+        is_owner = await self.check_owner()
+        
+        if not authorized:
+            await self.close()
+            return
+        
+        if not is_owner:
+            await self.close()
+            return
 
         try:
             state = text_data_json['state']
-            await self.update_state(state)
+            await self.update_state(state)  # is it needed?
             await self.channel_layer.group_send(self.device_room, {
                 'type': 'device_broadcast',
                 'state': state
             })
-        except KeyError:
-            pass
+        except Exception as e:
+            print("ERROR: ", e)
 
     # Receive message from room group
     async def device_broadcast(self, event):
         state = event['state']
 
         # Send message to WebSocket
-        await self.send(text_data=json.dumps({'state': state}))
+        if await self.authorize():  # dont need to call check_owner() here
+            await self.send(text_data=json.dumps({'state': state}))
 
     async def device_toggle(self, event):
-        """Handler for device.viewsets.toggle endpoint"""
+        """ Handler for device.viewsets.toggle endpoint """
         await self.send(text_data=json.dumps({'state': event['state']}))
 
     async def disconnect(self, close_code):
--- device/models.py
+++ device/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 from .choices import DEVICE_TYPE
 from place.models import Place
@@ -6,14 +7,14 @@ from place.models import Place
 
 class Device(models.Model):
     name = models.CharField(max_length=100)
-    type = models.CharField(choices=DEVICE_TYPE, max_length=20)
-    place = models.ForeignKey(Place, on_delete=models.CASCADE)
+    type = models.CharField(choices=DEVICE_TYPE, default="Generic", max_length=20)
+    #TODO: place = models.ForeignKey(Place, on_delete=models.CASCADE)
     state = models.BooleanField(
         default=False
-    )  # REPLACE IT WITH A `OneToMany` field, where each one is a port in the microcontroller. | this new model will has a field `default`.
+    )
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     channel = models.CharField(max_length=200, blank=True)
-
-    # mac = models.CharField(max_length=17, blank=True, unique=True)
+    mac = models.CharField(max_length=17, unique=True)
 
     def __str__(self):
-        return '{} - {}'.format(self.name, self.place)
+        return '{} - {}'.format(self.name, self.mac)
