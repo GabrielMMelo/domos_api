ae5b76f22dc1ea65d4742abd4291f6f2111ce0e3
Gabriel Marques de Melo <gabrielmelocomp@gmail.com>
2019-10-31 17:52:18 -0300

    Added CORS configuration to dev env

--- api_domos/development-settings.py
+++ api_domos/development-settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
@@ -49,6 +50,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -150,5 +152,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES':
     ('rest_framework.authentication.TokenAuthentication', )
 }
+
+# CORS
+# -----------------------------------------------------------------------------
+CORS_ORIGIN_ALLOW_ALL = True
 
 # ------------------------------------------------------------------------------
